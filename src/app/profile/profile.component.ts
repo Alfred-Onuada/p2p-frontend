6e38f6c7c355a2cb086a318a7526aac01e678eaf
Alfred-Onuada <aonuada5@gmail.com>
2023-06-03 10:47:33 +0100

    built profile component, now adding required modals

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  standalone: true,
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  imports: [CommonModule, MatCardModule, MatButtonModule]
+})
+export class ProfileComponent {
+  balance: number = 5000000;
+  firstName: string = "damilola"
+}
