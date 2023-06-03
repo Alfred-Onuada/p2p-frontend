5166daac07ac2d5b1e8ab27f230a231997d6ba96
Alfred-Onuada <aonuada5@gmail.com>
2023-06-03 23:02:03 +0100

    updated prod API link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "https://p2p-api.onrender.com",
+  apiUrl: "https://p2p-api.onrender.com/api",
   paystackPublicKey: "pk_test_f1bdf35e9e5ab3f840a73caaeefe6b6de5ed387d"
 };
